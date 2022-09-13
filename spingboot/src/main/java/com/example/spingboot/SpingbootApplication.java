6aca15e72e95d268aa4f786dd317ba44e702e11f
ECS <ECS@DESKTOP-048HU3O>
2022-09-13 11:32:35 +0900

    first commit

---
+++ spingboot/src/main/java/com/example/spingboot/SpingbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootApplication.class, args);
+	}
+
+}
